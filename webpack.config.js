2ed11369d751f130c3c3f8d2e2bc58590a00d2f6
ofelipelucca <felipe.lucca.t.oliveira@gmail.com>
2023-12-07 11:29:14 -0300

    Path webpack.config.js atualizado

--- webpack.config.js
+++ webpack.config.js
@@ -7,7 +7,7 @@ const dotenv = require('dotenv').config( {
 module.exports = {
   entry: "./src/dashboard.js",
   output: {
-    path: path.join(__dirname, 'index.html'),
+    path: path.resolve(__dirname),
     filename: "dashboard.js",
   },
   module: {
