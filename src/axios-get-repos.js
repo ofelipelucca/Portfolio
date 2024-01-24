c7527c1a5499bffe1bb844088edca0d71296ac20
ofelipelucca <felipe.lucca.t.oliveira@gmail.com>
2024-01-24 19:33:27 -0300

    Reformulação da requisição dos repositórios

---
+++ src/axios-get-repos.js
@@ -0,0 +1,42 @@
+const GITHUB_TOKEN = secrets.TOKEN_GITHUB;
+const GITHUB_USERNAME = secrets.USERNAME_GITHUB;
+
+const fs = require('fs');
+const axios = require('axios');
+
+// Fazendo a requisição das informações de todos os meus repositórios públicos
+axios.get(`https://api.github.com/users/${GITHUB_USERNAME}/repos?type=all&sort=updated&direction=desc`, {
+    headers: {
+        'Authorization': `token ${GITHUB_TOKEN}`
+    }
+})
+.then(response => {
+
+    // Obtendo os dados da resposta
+    const AXIOS_DATA = response.data;
+    
+    // Filtrando os dados por segurança
+    const FILTERED_DATA = AXIOS_DATA.map(repo => ({
+        name: repo.name,
+        description: repo.description,
+        created_at: repo.created_at,
+        updated_at: repo.updated_at,
+        stargazers_count: repo.stargazers_count,
+        language: repo.language
+    }));
+
+    // Convertendo os dados para formato JSON
+    const JSON_DATA = JSON.stringify(FILTERED_DATA, null, 2);
+
+    // Armazenando os dados em um arquivo JSON 
+    fs.writeFile('data/data.json', JSON_DATA, 'utf8', (err) => {
+        if (err) {
+            console.error('Erro ao escrever no arquivo:', err);
+        } else {
+            console.log('Os dados foram armazenados em dados.json com sucesso!');
+        }
+    });
+})
+.catch(error => {
+    console.error('Erro na requisição pelo axios:', error);
+});
---
+++ src/dashboard.js
@@ -0,0 +1,20 @@
+document.addEventListener('DOMContentLoaded', function() { 
+    
+    const REPOS_DIV = document.getElementById('repos');
+
+    // Criando uma div no html para exibir a imagem de avatar do usuário!
+    const AVATAR_URL = "https://avatars.githubusercontent.com/u/128730767?v=4";
+    const REPOS_AVATAR = document.createElement('div');
+
+    REPOS_AVATAR.innerHTML = `<a target="_blank" href="https://github.com/ofelipelucca"><img id="user-avatar" src="${AVATAR_URL}" alt="Foto de Perfil"></a>`;
+
+    REPOS_DIV.appendChild(REPOS_AVATAR);
+
+    // Criando uma div no html para conter a lista de repositórios
+    const REPOS_LIST = document.createElement('div');
+    REPOS_LIST.classList.add('repos-list');
+
+    REPOS_DIV.appendChild(REPOS_LIST);
+
+    // Ordenando todos os repositórios por data de criação
+});
