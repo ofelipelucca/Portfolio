dbb2fdf756f91eef036722745e5810d0eb01edd6
ofelipelucca <felipe.lucca.t.oliveira@gmail.com>
2026-03-07 12:10:43 -0300

    feat: Integração com Google Analytics

---
+++ src/lib/gtag.ts
@@ -0,0 +1,15 @@
+export const GA_MEASUREMENT_ID = process.env.NEXT_PUBLIC_GA_ID || "";
+
+declare global {
+    interface Window {
+        gtag: (...args: any[]) => void;
+    }
+}
+
+export const pageview = (url: string) => {
+    if (typeof window.gtag !== "undefined") {
+        window.gtag("config", GA_MEASUREMENT_ID, {
+            page_path: url,
+        });
+    }
+};
