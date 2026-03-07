9f8c758474840cd2cbaebe0d9d5b9ee0003d336f
Felipe Lucca Taumaturgo de Oliveira <felipe.lucca.t.oliveira@gmail.com>
2026-03-07 17:01:51 -0300

    refactor: Padronização de código (#13)
    * refactor: Padronização de código
    
    * refactor: Padronização de código

--- src/lib/gtag.ts
+++ src/lib/gtag.ts
@@ -1,15 +1,13 @@
-export const GA_MEASUREMENT_ID = process.env.NEXT_PUBLIC_GA_ID || "";
-
+export const GA_MEASUREMENT_ID = process.env.NEXT_PUBLIC_GA_ID || ""
 declare global {
-    interface Window {
-        gtag?: (command: 'config' | 'event' | 'set', targetId: string, params?: Record<string, unknown>) => void;
-    }
+  interface Window {
+    gtag?: (command: 'config' | 'event' | 'set', targetId: string, params?: Record<string, unknown>) => void
+  }
 }
-
 export const pageview = (url: string) => {
-    if (typeof window.gtag !== "undefined") {
-        window.gtag("config", GA_MEASUREMENT_ID, {
-            page_path: url,
-        });
-    }
-};
+  if (typeof window.gtag !== "undefined") {
+    window.gtag("config", GA_MEASUREMENT_ID, {
+      page_path: url,
+    })
+  }
+}
